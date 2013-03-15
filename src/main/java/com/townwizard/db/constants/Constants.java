b4c36cb1805a0b6c2fcef556b1608f9dc5dc207d
Vladimir Mazheru <vmazheru@salzinger.com>
2013-03-15 13:10:01 -0400

    Changed Twitter key and secret

--- src/main/java/com/townwizard/db/constants/Constants.java
+++ src/main/java/com/townwizard/db/constants/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
     public static final String FB_APP_ID = "318928931561066";
     public static final String FB_APP_SECRET = "db2ade8f8cac734d2571547c3fded645";
     public static final String FB_LOGIN_RESOURCE = "http://www.townwizardconnect.com/login/fb";    
-    public static final String TWITTER_APP_ID = "b7ZXg9lYW5OhQ3InvnJQ";
-    public static final String TWITTER_APP_SECRET = "DZATEfUCCel270ARGcYdHTOWD6a1NHxu74TRCGHHjHg";
+    public static final String TWITTER_APP_ID = "ipFojnBOlje5a7BzVrJBg";
+    public static final String TWITTER_APP_SECRET = "sYaSRr18NtUdn4bCEkr8Qq8aEvZzXEsERtkNyINj7o";
     public static final String TWITTER_LOGIN_RESOURCE = "http://www.townwizardconnect.com/login/twitter";
     public static final String MEETUP_API_KEY = "441c7748257a4416672778255838a7d";
     public static final String PHP_LOGIN_PATH = "/townwizard-db-api/fb-login.php";
