782c9292529b54960df06e904fbfeb1c204b9b3f
Vladimir Mazheru <vmazheru@salzinger.com>
2013-05-08 13:40:08 -0400

    Timing debug statements

--- src/main/java/com/townwizard/db/resources/GlobalDataResource.java
+++ src/main/java/com/townwizard/db/resources/GlobalDataResource.java
@@ -89,7 +89,10 @@ public class GlobalDataResource extends ResourceSupport {
     @Produces(MediaType.APPLICATION_JSON)
     public Response placeCategories(@QueryParam ("cat") String mainCategory) {
         try {
+            long start = System.currentTimeMillis();
             List<String> categories = globalDataService.getPlaceCategories(mainCategory);
+            long end = System.currentTimeMillis();
+            Log.debug("Served categories in " + (end - start) + " ms");
             return Response.status(Status.OK).entity(categories).build();
         } catch(Exception e) {
             handleGenericException(e);
--- src/main/java/com/townwizard/globaldata/service/provider/FacebookServiceImpl.java
+++ src/main/java/com/townwizard/globaldata/service/provider/FacebookServiceImpl.java
@@ -12,7 +12,6 @@ import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.townwizard.db.logger.Log;
 import com.townwizard.db.util.CollectionUtils;
 import com.townwizard.db.util.JSONUtils;
 import com.townwizard.globaldata.connector.FacebookConnector;
@@ -82,7 +81,6 @@ public class FacebookServiceImpl implements FacebookService {
             if(first) first = false;
         }
         String fql = sb.toString();
-        if(Log.isDebugEnabled()) Log.debug(fql);
         return fql;
     }
 
